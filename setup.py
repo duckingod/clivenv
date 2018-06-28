653514d2e67587d1489c3a2365a310f8d7110402
duckingod <kingoduck@gmail.com>
2018-06-28 11:36:45 +0800

    Zsh Compatibility

--- setup.py
+++ setup.py
@@ -5,20 +5,19 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="cli_venv",
-    version="0.0.1",
+    name="clivenv",
+    version="0.0.2",
     author="duckingod",
     author_email="kingoduck@gmail.com",
     description="Fast access/manage to virtualenv between different python versions.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/duckingod/cli_venv",
+    url="https://github.com/duckingod/clivenv",
     packages=setuptools.find_packages(),
-    install_requires=[ 'argparse', 'virtualenv' ],
+    install_requires=['virtualenv'],
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
         "Environment :: Console"
     ),
     scripts=[join('venv', 'venv'), join('venv', 'venv.py'), join('venv', 'venv_pip_recording')],
