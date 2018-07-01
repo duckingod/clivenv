957986f9555dfb04ca8333731152cc48aa6e96e1
duckingod <kingoduck@gmail.com>
2018-07-01 21:30:14 +0800

    Version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="clivenv",
-    version="0.0.4",
+    version="0.0.5",
     author="duckingod",
     author_email="kingoduck@gmail.com",
     description="Fast access/manage to virtualenv between different python versions.",
